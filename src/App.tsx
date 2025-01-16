563aee72163749c367ce0db094dbe011650190ba
Kevin Cepria <kevin.cepria@gmail.com>
2025-01-17 07:08:05 +0800

    remove websocket implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
 import { AudioRecorder } from "react-audio-voice-recorder";
 import { convertWebmToWav } from "./uitls/audio";
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const containerRef = useRef<HTMLDivElement | null>(null);
   const [blob, setBlob] = useState<Blob | null>(null);
-  const websocketRef = useRef<WebSocket | null>(null);
-
-  useEffect(() => {
-    // Create and open the WebSocket connection
-    const ws = new WebSocket("wss://echo.websocket.org");
-    websocketRef.current = ws;
-
-    // On connection success
-    ws.onopen = () => {
-      console.log("WebSocket connection established.");
-    };
-
-    ws.onmessage = (event) => {
-      console.log(`Received from server: ${event.data}`);
-    };
-
-    ws.onerror = (error) => {
-      console.log("WebSocket error occurred.");
-      console.error("WebSocket error:", error);
-    };
-
-    ws.onclose = () => {
-      console.log("WebSocket connection closed.");
-    };
-
-    // Cleanup on unmount
-    return () => {
-      ws.close();
-    };
-  }, []);
 
   const addAudioElement = async (blob: Blob) => {
     const { wavBlob, l16Blob } = await convertWebmToWav(blob);
@@ -44,22 +13,19 @@ const App = () => {
   const sendAudioBlobToApi = async () => {
     if (!blob) return;
     try {
-      // Create a FormData object and append the blob
       const formData = new FormData();
       formData.append("audioFile", blob);
 
-      // Send the request to the API using fetch
       const response = await fetch("url", {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${"token"}`, // Bearer Token in the Authorization header
+          Authorization: `Bearer ${"token"}`,
         },
-        body: formData, // Form data contains the Blob
+        body: formData,
       });
 
-      // Handle the response
       if (response.ok) {
-        const result = await response.json(); // Parse the response body as JSON
+        const result = await response.json();
         console.log("File uploaded successfully:", result);
       } else {
         console.error("Upload failed:", response.status, await response.text());
@@ -69,21 +35,8 @@ const App = () => {
     }
   };
 
-  const sendAudioBlobToWS = () => {
-    if (
-      websocketRef.current &&
-      websocketRef.current.readyState === WebSocket.OPEN &&
-      blob
-    ) {
-      websocketRef.current.send(blob);
-      console.log("Sending data");
-    } else {
-      console.log("WebSocket is not connected.");
-    }
-  };
-
   return (
-    <div className="container" ref={containerRef}>
+    <div className="container">
       <AudioRecorder
         onRecordingComplete={addAudioElement}
         audioTrackConstraints={{
@@ -96,12 +49,9 @@ const App = () => {
       {blob && (
         <>
           <audio src={URL.createObjectURL(blob)} controls={true} />
-          <div className="action-container">
-            <button onClick={sendAudioBlobToWS}>Send To WS</button>
-            <button onClick={async () => await sendAudioBlobToApi()}>
-              Send To REST API
-            </button>
-          </div>
+          <button onClick={async () => await sendAudioBlobToApi()}>
+            Send To REST API
+          </button>
         </>
       )}
     </div>
